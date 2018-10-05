bbe2ee699f4d336c847a9ce5b321716a6275aed4
LanZBoY <superjimalex@gmail.com>
2018-10-05 20:23:21 +0800

    123

--- src/main.h
+++ src/main.h
@@ -3,10 +3,6 @@
 
 int addNumber(int a, int b);
 int getTriangleType(int a, int b, int c);
-<<<<<<< HEAD
 int mux(int a,int b);
-=======
 int addnumber2(int a, int b);
->>>>>>> master
-
 #endif // MAIN_H_INCLUDED
--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -7,11 +7,8 @@ void testGetTriangleType() {
 
 int main() {
     assert(addNumber(5, 2) == 7);
-<<<<<<< HEAD
     assert(mux(5,2)==10);
-=======
     assert(addnumber2(5, 2) == 4);
->>>>>>> master
     testGetTriangleType();
     return 0;
 }
